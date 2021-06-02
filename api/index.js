de38d14c8f0d1fe363de427008e7f1ae92aea82e
THiragi <w1th1nmybr41n@gmail.com>
2021-06-03 00:32:23 +0900

    remove console.log

--- api/index.js
+++ api/index.js
@@ -23,7 +23,6 @@ export default async (req, res) => {
       })
       .then((text) => {
         const dom = new JSDOM(text);
-        console.log(dom);
         const meta = dom.window.document.querySelectorAll(`head > meta`);
         return Array.from(meta)
           .filter((el) => el.hasAttribute('property'))
@@ -32,14 +31,13 @@ export default async (req, res) => {
               .getAttribute('property')
               .trim()
               .replace('og:', '');
-            console.log(property);
             const content = ogp.getAttribute('content');
 
             return { ...pre, [property]: content };
           }, {});
       });
 
-    // res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
 
     return res.status(200).json(result);
   } catch (error) {
