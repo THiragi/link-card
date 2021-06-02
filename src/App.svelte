0b6cd8679ead98a81ecdb5c83cadcb920e598520
THiragi <w1th1nmybr41n@gmail.com>
2021-06-03 00:32:32 +0900

    add some styles

--- src/App.svelte
+++ src/App.svelte
@@ -16,19 +16,74 @@
   onMount(async () => {
     try {
       const result = await fetcher<Meta>(`/api?url=${url}`);
-      console.log(result)
       metaData = result;
     } catch (error) {
-      console.log(error);
       metaData = {};
     }
     
   });
 </script>
 
-<main>
-  <h1>URL</h1>
-  <p>{url}</p>
-  <h1>TITLE</h1>
-  <p>{metaData?.title}</p>
-</main>
+<div class="container">
+
+  {#if metaData}
+    <a href={url}
+      rel="nofollow noreferrer noopener"
+      target="_blank"
+    >
+      <div class="info">
+        <h1>{metaData?.title}</h1>
+      </div>
+      {#if metaData?.description}
+        <div>{metaData.description}</div>
+      {/if}
+  
+  
+      {#if metaData?.image}
+        <div class="thumbnail">
+          <img 
+            src={metaData.image}
+            alt=""
+          />
+        </div>
+        
+      {/if}
+    </a>
+  {/if}
+</div>
+
+<style>
+  .container {
+    max-width: 780px;
+    margin: 0 auto;
+    border: 1px solid #eaeaea;
+    border-radius: 5px;
+    overflow: hidden
+  }
+  a {
+    display: flex;
+    align-items: center;
+    font-size: 16.5px;
+    height: 120px;
+    line-height: 1.5;
+    transition: 0.2s;
+    color: rgba(0, 0, 0, 0.82);
+    text-decoration: none;
+    background: #fff;
+  }
+  a:hover {
+    background: rgba(239, 246, 251, 0.7);
+  }
+  .info {
+    flex: 1;
+    padding: 0.8em 1.2em;
+  }
+
+  h1 {
+    font-size: 1em;
+  }
+
+  .thumbnail > img {
+    height: 120px;
+  }
+</style>
